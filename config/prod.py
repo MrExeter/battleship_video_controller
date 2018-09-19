8a0ccc3db142ffdc4e27d22cf434852d95a02f53
MrExeter <exetor@mindspring.com>
2018-09-19 14:29:55 -0700

    Add production config

---
+++ config/prod.py
@@ -0,0 +1,9 @@
+DEBUG = False
+# SECRET_KEY = 'dbdeveloper'
+SERVER_NAME = '127.0.0.1:5000'
+localhost = SERVER_NAME
+SECRET_KEY = 'applejack'
+WTF_CSRF_SECRET_KEY = 'applejack'
+CSRF_SESSION_KEY = 'applejack'
+SQLALCHEMY_DATABASE_URI = 'postgresql://dbdeveloper:dbdeveloper@localhost/prod_battleship_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
